49f2759f9d1cdc3ce45490a318eb83bffc691ac7
inayadiza <mainah.diza@gmail.com>
2024-11-06 05:11:26 +0700

    coba push

---
+++ android/app/src/main/kotlin/com/example/coree_elegante/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.coree_elegante
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:coree_elegante/menu.dart';
+
+// Membuat MaterialColor kustom untuk warna hitam
+MaterialColor createMaterialBlack = MaterialColor(
+  Colors.black.value,
+  const <int, Color>{
+    50: Color(0xFF000000),
+    100: Color(0xFF000000),
+    200: Color(0xFF000000),
+    300: Color(0xFF000000),
+    400: Color(0xFF000000),
+    500: Color(0xFF000000),
+    600: Color(0xFF000000),
+    700: Color(0xFF000000),
+    800: Color(0xFF000000),
+    900: Color(0xFF000000),
+  },
+);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        primarySwatch: createMaterialBlack,
+        colorScheme: ColorScheme.dark(
+          primary: Colors.black,
+          secondary: Colors.black,
+          surface: Colors.black,
+          background: Colors.white,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
